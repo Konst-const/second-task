f293393c41d7f9044a03350d53418a1fcf5090ea
kons.const.vll <kons.const.vll@gmail.com>
2021-02-10 23:21:37 +0300

    Выполнен 2 пункт задания №2

--- 2.php
+++ 2.php
@@ -13,3 +13,54 @@ function convertString(string &$a, string $b): void
 //$a = ' hello Hello Dev Hello hello';
 //convertString($a, 'Hello');
 //var_dump($a);
+
+function bubbleSort(array &$array, \Closure $compare)
+{
+    for ($i = 0; $i < count($array); $i++) {
+        for ($y = ($i+1); $y < count($array); $y++) {
+            if ($compare($array[$i], $array[$y], $i, $y)) {
+                $c = $array[$i];
+                $array[$i] = $array[$y];
+                $array[$y] = $c;
+            }
+        }
+    }
+}
+
+function mySortForKey(array $a, $b): array
+{
+    bubbleSort($a, function (array $one, array $two, $oneIndex, $twoIndex) use ($b) {
+        if (! isset($one[$b])) {
+            throw new Exception('Отсутствует ключ '. $b . ' в элементе под индексом: '. $oneIndex);
+        }
+
+        if (! isset($two[$b])) {
+            throw new Exception('Отсутствует ключ '. $b . ' в элементе под индексом: '. $twoIndex);
+        }
+
+        return $one[$b] > $two[$b];
+    });
+
+    return $a;
+}
+
+$array = [
+    [
+        'a' => 1,
+        'b' => 2,
+    ],
+    [
+        'a' => 10,
+        'b' => 3,
+    ],
+    [
+        'a' => 3,
+        'b' => 4,
+    ],
+    [
+        'a' => 97,
+        'b' => 0,
+    ],
+];
+
+var_dump(mySortForKey($array, 'a'));
